7660befaca787277b943d123d3529a4bf871f030
sairam <sairamnaidu64@gmail.com>
2021-07-20 16:51:02 +0530

    add print line

--- src/main/java/com/example/demo/main/Welcom.java
+++ src/main/java/com/example/demo/main/Welcom.java
@@ -3,6 +3,7 @@ package com.example.demo.main;
 public class Welcom {
 	public static void main(String[] args) {
 		System.out.println("Welcom To Our GitHub Store.....");
+		System.out.println("Gihub");
 	}
 
 }
